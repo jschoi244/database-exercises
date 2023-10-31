7603f022a83d56064bf9f05c820c1c3ca0f0f2f5
Justin Choi <justinchoi244@gmail.com>
2023-10-31 11:04:10 -0500

    first commit

---
+++ src/main/resources/albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(50) DEFAULT 'NONE',
+    release_date INT UNSIGNED,
+    sales FLOAT,
+    genre VARCHAR(50) DEFAULT 'NONE'
+);
